efe471180cfef43904dfb289ed5e3bd614c6b26b
Daisuke Sakurai <d.sakurai0@gmail.com>
2025-06-21 17:14:29 +0900

    Clean up

--- rake.sh
+++ rake.sh
@@ -5,9 +5,8 @@ set -e
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_PATH"
 
-export SERVICE_NAME="rake_service"
-
 podman-compose build --pull
 
 export HOST_WORKDIR="$SCRIPT_PATH"
-podman-compose run --rm $SERVICE_NAME "$@"
+podman-compose run --rm rake_service "$@"
+# rake_service is the name of the service defined in the podman-compose.yml file
